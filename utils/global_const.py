6983315ede6b65d9f6d697e6e2ada0350ab645dd
Zeyu Yang <zeyuyang8@gmail.com>
2023-02-24 00:57:12 -0600

    Add files.

---
+++ utils/global_const.py
@@ -0,0 +1,33 @@
+''' 
+Constants for the project 
+'''
+from PIL import Image
+
+# Resource paths
+PLOTS_PATH = "plots/"
+DATA_PATH = "/Users/zeyuyang/Datasets/Audubon/"
+UTILS_PATH = "utils/"
+
+# Constants saving information
+FILES = {}
+FRAMES = {}
+
+# Fixed parameters
+SAVE_FIG = True
+Image.MAX_IMAGE_PIXELS = 1000000000
+DPI = 1000
+COL_NAMES = ["class_id", "class_name", "x", "y", "width", "height"]
+GROUPS = {"BRPE": ["BRPEA", "BRPEC", "BRPEJ", "BRPEW", "BRPEF"],
+          "LAGU": ["LAGUA", "LAGUF", "LAGUJ"],
+          "BLSK": ["BLSKA", "BLSKF"],
+          "MTRN": ["MTRNA", "MTRNF", "AMAVA", "AMOYA"],
+          "LGHT": ["GREGA", "GREGC", "GREGF", "SNEGA", "REEGWMA", 
+              "WHIBA", "WHIBC", "WHIBN", "WHIBJ", "ROSPA", "TCHEA", "MEGRT",
+              "CAEGA", "CAEGF"],
+          "DARK": ["GBHEA", "GBHEC", "GBHEJ", "GBHEN", "GBHEE", "GBHEF",
+              "REEGA", "REEGF", "REEGC", "TRHEA", "BCNHA", "DCCOA"], 
+          "OTHR": ["OTHRA"],
+          "TRSH": ["TRASH"],
+        }
+# I put "REEGA", "REEGF", "REEGC" into "DARK" but I am not sure
+# Not sure about "CAEGA", "CAEGF", "MEGRT", "TCHEA", "AMAVA", "DCCOA", "AMOYA"
